4cf8f6878fdce6cff9798edda2ad1bc3f96923d2
Bart <bartolomej.kozorog@gmail.com>
2023-05-10 23:42:22 +0200

    different spiral pattern, dynamic color schemes,...

--- src/sketches/spiral.js
+++ src/sketches/spiral.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const chroma = require("chroma-js");
 
 const settings = {
   duration: 3,
@@ -10,41 +11,50 @@ const settings = {
 };
 
 const sketch = ({ width, height, context }) => {
-  context.canvas.style.background = 'black';
+  context.canvas.style.background = chroma.random().alpha(0.1);
 
-  const nPoints = 120;
-  const trailLength = 15;
-  const spacingFactor = 8;
+  const totalPoints = 300;
+  const trailLength = 30;
+  const spacingFactor = 10;
   const pointSize = 8;
   const start = { x: width / 2, y: height / 2 }
-  const dTime = 0.025;
-  const randomness = 0;
+  const dTime = 0.01;
+  const randomnessFactor = 0;
   let time = 0;
 
-  const color = () => `hsl(40, ${Math.sin(time * 2 + 15.7) * 100}%, ${Math.cos(time * 2) * 40 + 60}%)`;
+  const colorScheme = chroma
+      .bezier([chroma.random(), chroma.random()])
+      .scale()
+      .colors(trailLength)
+      .map(color => chroma(color).darken());
+
   const spacing = () => Math.cos(time) * spacingFactor;
-  const a = () => Math.pow(Math.sin(time), 2);
-  const posX = i => start.x + Math.cos(i + time + a()) * spacing() * i;
-  const posY = i => start.x + Math.sin(i + time + a()) * spacing() * i;
+  const posX = i => start.x + Math.cos(i * Math.sqrt(time)) * spacing() * i;
+  const posY = i => start.x + Math.sin(i * Math.sqrt(time)) * spacing() * i;
 
   const points = [];
-  for (let i = 0; i < nPoints; i++) {
+  for (let i = 0; i < totalPoints; i++) {
     points.push({ x: posX(0), y: posY(0), trail: [] });
   }
 
   return ({ context: ctx, width, height }) => {
     ctx.clearRect(0, 0, width, height);
 
-    ctx.beginPath();
     for (let i = 0; i < points.length; i++) {
       let { x, y, trail } = points[i];
-      ctx.moveTo(x, y);
-      ctx.arc(x, y, pointSize, 0, Math.PI * 2);
 
       for (let j = 0; j < trail.length; j++) {
         let { x, y } = trail[j];
+
+        ctx.beginPath();
         ctx.moveTo(x, y);
         ctx.arc(x, y, pointSize * (j / trail.length), 0, Math.PI * 2);
+
+        const progressTowardsEndPoint = j / points.length
+
+        ctx.closePath()
+        ctx.fillStyle = chroma(colorScheme[j]).alpha(progressTowardsEndPoint * 3);
+        ctx.fill();
       }
 
       trail.push(Object.assign({}, { x, y }));
@@ -52,13 +62,11 @@ const sketch = ({ width, height, context }) => {
         trail.splice(0, 1);
       }
 
-      const rand = () => ((Math.random() - 0.5) * Math.sin(time)) * randomness;
+      const randomStep = () => ((Math.random() - 0.5) * Math.sin(time)) * randomnessFactor;
 
-      points[i].x = posX(i) + rand();
-      points[i].y = posY(i) + rand();
+      points[i].x = posX(i) + randomStep();
+      points[i].y = posY(i) + randomStep();
     }
-    ctx.fillStyle = color();
-    ctx.fill();
 
     time += dTime;
   };
